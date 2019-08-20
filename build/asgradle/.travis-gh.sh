e95c4fafda7efe4bd76d18d8fd8eb5bcf7b86c89
flambard-took <flambard@mangband.org>
2019-08-20 17:02:32 +0000

    More tinkering.

---
+++ build/asgradle/.travis-gh.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+CLIENT_D=build/asgradle/app/build/outputs/debug/mangclient-debug.apk
+CLIENT_R=build/asgradle/app/build/outputs/release/mangclient-release-unsigned.apk
+
+CLIENT_D_NAME=$(basename ${CLIENT_D})
+CLIENT_R_NAME=$(basename ${CLIENT_R})
+
+cat <<"HTML"
+<html>
+<title>MAngband Android builds</title>
+<head><meta charset="utf-8"/></head>
+<body>
+HTML
+echo "<p><a href='${CLIENT_D}'>${CLIENT_D_NAME}</a></p>"
+echo "<p><a href='${CLIENT_R}'>${CLIENT_R_NAME}</a></p>"
+echo Built on $(date)
+cat <<"HTML"
+</body>
+</html>
+HTML
