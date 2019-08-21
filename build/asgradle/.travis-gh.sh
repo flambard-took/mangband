b89b154427644df1a5dc2fab883d3374eeefe851
flambard-took <flambard@mangband.org>
2019-08-21 07:02:45 +0000

    Add qrcodes to page.

--- build/asgradle/.travis-gh.sh
+++ build/asgradle/.travis-gh.sh
@@ -6,13 +6,34 @@ CLIENT_R=mangclient-release-unsigned.apk
 cat <<"HTML"
 <html>
 <title>MAngband Android builds</title>
-<head><meta charset="utf-8"/></head>
-<body>
+<head><meta charset="utf-8"/>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/qrcode-generator/1.4.3/qrcode.min.js"></script>
+<style>div.qr:nth-of-type(even) { text-align: right; }</style>
+</head>
+<body onload="makecodes()">
 HTML
-echo "<p><a href='${CLIENT_D}'>${CLIENT_D}</a></p>"
-echo "<p><a href='${CLIENT_R}'>${CLIENT_R}</a></p>"
+echo "<div class='qr'><a href='${CLIENT_D}'>${CLIENT_D}</a><div></div></div>"
+echo "<div class='qr'><a href='${CLIENT_R}'>${CLIENT_R}</a><div></div></div>"
 echo Built on $(date)
 cat <<"HTML"
+<script>
+function makecodes() {
+var os = document.querySelectorAll('div.qr');
+var ls = document.querySelectorAll('div.qr a');
+var ps = document.querySelectorAll('div.qr div');
+var i = 0;
+for (i = 0; i < os.length; i++) {
+    var d = os[i];
+    var a = ls[i];
+    var p = ps[i];
+    var href = a.href;
+    alert(href);
+    var qr = qrcode(4, 'L');
+    qr.addData(href); qr.make();
+    p.innerHTML = qr.createImgTag(5);
+}
+}
+</script>
 </body>
 </html>
 HTML
